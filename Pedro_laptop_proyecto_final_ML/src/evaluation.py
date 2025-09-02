c7c5ca1e7043c5ee61ba6316992e6513e37b9e14
Pedropas95 <drulifero@gmail.com>
2025-09-02 22:09:09 +0200

    Update evaluation.py

--- Pedro_laptop_proyecto_final_ML/src/evaluation.py
+++ Pedro_laptop_proyecto_final_ML/src/evaluation.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
-
-import subprocess, sys
+import sys, subprocess
 from pathlib import Path
 
-def evaluar():
-    repo = Path(__file__).resolve().parent.parent
-    nb3 = repo / "notebooks" / "03_Entrenamiento_Evaluacion.ipynb"
-    assert nb3.exists(), f"No encuentro {nb3}"
-    cmd = [sys.executable, "-m", "jupyter", "nbconvert", "--to", "notebook", "--execute", "--inplace", str(nb3)]
-    print("[eval] Ejecutando:", nb3)
-    subprocess.check_call(cmd, cwd=str(repo))
-    print("[eval] Hecho:", nb3)
+repo = Path(__file__).resolve().parent.parent
+nb3 = repo / "notebooks" / "03_Entrenamiento_Evaluacion.ipynb"
 
-if __name__ == "__main__":
-    evaluar()
+print("Ejecutando evaluación:", nb3)
+subprocess.check_call([sys.executable, "-m", "jupyter", "nbconvert",
+                       "--to", "notebook", "--execute", "--inplace", str(nb3)],
+                      cwd=str(repo))
+print("Hecho:", nb3)
